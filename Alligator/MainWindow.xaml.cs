6f0e054aeb13760633a829e30e1f027ea4cdd414
Polina <rudichrudichrudich@mail.ru>
2021-12-08 23:13:38 +0300

    deleted notices

--- Alligator/MainWindow.xaml.cs
+++ Alligator/MainWindow.xaml.cs
@@ -27,44 +27,6 @@ namespace Alligator
             InitializeComponent();
           
         }
-        public static SqlConnection GetConnection(string param = "Data Source=.;Initial Catalog=AggregatorAlligator;Integrated Security=True;")
-        {
-            SqlConnection connection = new SqlConnection(param);
-            return connection;
-        }
 
-        public static void InsertClient(int id, string fName, string sName, string tName, string phoneNumber, string email)
-        {
-            SqlConnection connection = GetConnection();
-            connection.Open();
-            SqlCommand command;
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            string sql = "";
-
-            sql = $"insert into [dbo].[Client] (Id, FirstName, LastName, Patronymic, PhoneNumber, Email) values ({id},'{fName}','{sName}', '{tName}','{phoneNumber}','{email}')";
-            command = new SqlCommand(sql, connection);
-
-            adapter.InsertCommand = command;
-            adapter.InsertCommand.ExecuteNonQuery();
-
-            command.Dispose();
-            connection.Close();
-        }
-        public static void SelectClient()
-        {
-            SqlConnection connection = GetConnection();
-            connection.Open();
-            SqlCommand command;
-            SqlDataReader dataReader;
-            string sql, output = "";
-            sql = "Select Id,FirstName,LastName,Patronymic,honeNumber,Email from Client";
-            command = new SqlCommand(sql, connection);
-            dataReader = command.ExecuteReader();
-            while(dataReader.Read())
-            {
-
-            }
-        }
-       
     }
 }
