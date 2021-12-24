62cce8843f8acd588a1d86edcf049cf63d223420
Polina <rudichrudichrudich@mail.ru>
2021-12-24 19:44:56 +0300

    forgot to save service

--- Alligator.BusinessLayer/Services/CommentService.cs
+++ Alligator.BusinessLayer/Services/CommentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Alligator.DataLayer.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Alligator.BusinessLayer.Services
 {
-    class CommentService
+    public class CommentService
     {
+        private readonly CommentRepository _commentRepository;
+        public CommentService()
+        {
+            _commentRepository = new CommentRepository();
+        }
     }
 }
